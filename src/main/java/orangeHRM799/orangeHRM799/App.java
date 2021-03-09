5fb2cdf78f6e8ed585fe9221767ac212039dd87c
Ramesh <rameswarudu.naraboina@team.telstra.com>
2021-03-09 12:12:28 +0530

    commit and push by murali

---
+++ src/main/java/orangeHRM799/orangeHRM799/App.java
@@ -0,0 +1,13 @@
+package orangeHRM799.orangeHRM799;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
